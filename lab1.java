60338e65eab8efa278346745e072d7ea5c9f75ec
Poptergeyts <75073374+Poptergeyts@users.noreply.github.com>
2022-09-12 08:36:55 +0300

    Исправил нейминг

--- lab1.java
+++ lab1.java
@@ -6,19 +6,19 @@ public class lab1 {
         float[] x = new float[11];                                                //одномерный массив x типа float
         double[][] c = new double[s.length][x.length];                            //двумерный массив c типа double
 
-        byte indxI = 0;                                                           //переменная для индекса первого порядка
+        byte indexI = 0;                                                          //переменная для индекса первого порядка
         for (byte elForS = 16; elForS > 3; elForS--){                             //цикл для заполнения массива s числами от 16 до 4 включительно
-            s[indxI++] = elForS;
+            s[indexI++] = elForS;
         }
-        for (indxI = 0; indxI < x.length; indxI++){                               //цикл для заполнения массива x 11ю рандомными числами от -15.0 до 14.0
-            x[indxI] = (float)(Math.random() * (14 - -15) + -15);                 //метод random генерирует рандомное чило от 0.0 до 1.0
+        for (indexI = 0; indexI < x.length; indexI++){                            //цикл для заполнения массива x 11ю рандомными числами от -15.0 до 14.0
+            x[indexI] = (float)(Math.random() * (14 - -15) + -15);                //метод random генерирует рандомное чило от 0.0 до 1.0
         }
 
-        for (indxI = 0; indxI < c.length; indxI++){                               //вложенный цикл для заполнения массива c
-            for (byte indxJ = 0; indxJ < c[indxI].length; indxJ++){
-                switch(s[indxI]){                                                 //оператор switch для проверки значения s[indxI]
+        for (indexI = 0; indexI < c.length; indexI++){                            //вложенный цикл для заполнения массива c
+            for (byte indexJ = 0; indexJ < c[indexI].length; indexJ++){
+                switch(s[indexI]){                                                //оператор switch для проверки значения s[indexI]
                     case 6:
-                        c[indxI][indxJ] = Math.tan(Math.pow(Math.pow(x[indxJ], (1.0 / 3.0)), (2 / Math.pow((((1.0 / 2.0) - x[indxJ]) / x[indxJ]), x[indxJ]))));
+                        c[indexI][indexJ] = Math.tan(Math.pow(Math.pow(x[indexJ], (1.0 / 3.0)), (2 / Math.pow((((1.0 / 2.0) - x[indexJ]) / x[indexJ]), x[indexJ]))));
                         break;
                     case 4:
                     case 5:
@@ -26,20 +26,20 @@ public class lab1 {
                     case 9:
                     case 10:
                     case 12:
-                        c[indxI][indxJ] = Math.pow(Math.exp(Math.pow(x[indxJ], (2 * x[indxJ]))), (1.0 / 3.0));
+                        c[indexI][indexJ] = Math.pow(Math.exp(Math.pow(x[indexJ], (2 * x[indexJ]))), (1.0 / 3.0));
                         break;
                     default:
-                        c[indxI][indxJ] = Math.atan(1.0 / Math.exp(Math.pow(Math.pow(Math.tan(Math.asin(Math.sin(x[indxJ]))), 2)
-                                        * (2 * Math.PI + Math.pow(Math.tan(Math.sin(Math.tan(x[indxJ]))), 2)),
-                                (Math.log(Math.pow((4 + Math.pow((Math.PI / Math.abs(x[indxJ])), x[indxJ])),
-                                        (Math.asin(1.0 / Math.exp(Math.abs(x[indxJ]))))))))));
+                        c[indexI][indexJ] = Math.atan(1.0 / Math.exp(Math.pow(Math.pow(Math.tan(Math.asin(Math.sin(x[indexJ]))), 2)
+                                        * (2 * Math.PI + Math.pow(Math.tan(Math.sin(Math.tan(x[indexJ]))), 2)),
+                                (Math.log(Math.pow((4 + Math.pow((Math.PI / Math.abs(x[indexJ])), x[indexJ])),
+                                        (Math.asin(1.0 / Math.exp(Math.abs(x[indexJ]))))))))));
                         break;
                 }
             }
         }
-        for (indxI = 0; indxI < c.length; indxI++) {                             //вложенный цикл для вывода массива c
-            for (byte indxJ = 0; indxJ < c[indxI].length; indxJ++) {
-                System.out.printf("%.3f ", c[indxI][indxJ]);                     //%.3f - форматирует в число с тремя знаками после запятой
+        for (indexI = 0; indexI < c.length; indexI++) {                            //вложенный цикл для вывода массива c
+            for (byte indexJ = 0; indexJ < c[indexI].length; indexJ++) {
+                System.out.printf("%.3f ", c[indexI][indexJ]);                     //%.3f - форматирует в число с тремя знаками после запятой
             }
             System.out.println();
         }
